08f259ba53d5d8f3544d49faa86ee843c17b370b
KEZEIWEI <C111181123@nkust.edu.tw>
2024-12-30 21:47:55 +0800

    copy new template

--- db.php
+++ db.php
@@ -1,14 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "seat_booking";
+// db.php - 只在這裡開啟一次連接
+$servername = "13.54.4.168"; // 資料庫伺服器
+$username = "root";        // 資料庫用戶名
+$password = "root";            // 資料庫密碼
+$dbname = "seat_booking";  // 資料庫名稱
 
-// 建立資料庫連接
+// 創建連接
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 檢查資料庫連接是否成功
+// 檢查連接是否成功
 if ($conn->connect_error) {
     die("資料庫連接失敗: " . $conn->connect_error);
 }
+
+// 不要在此處關閉連接，讓它在其他地方被正常使用
 ?>
